c797a08f86fb24cbf7879e76dc5c5002d7f467c1
Glenn Posadas <hello@glennvon.com>
2020-04-17 19:45:13 +0800

    add iboutlets.

--- Gitt/Gitt/View/Profile/ProfileViewController.swift
+++ Gitt/Gitt/View/Profile/ProfileViewController.swift
@@ -8,15 +8,47 @@
 
 import UIKit
 
+/**
+ The controller for profile screen.
+ */
 class ProfileViewController: BaseViewController {
 
     // MARK: - Properties
     
+    @IBOutlet weak var imageView_BG: UIImageView!
+    @IBOutlet weak var imageView_Banner: UIImageView!
+    
+    @IBOutlet weak var label_Followers: UILabel!
+    @IBOutlet weak var label_Following: UILabel!
+    @IBOutlet weak var label_Name: UILabel!
+    @IBOutlet weak var label_Company: UILabel!
+    @IBOutlet weak var label_Blog: UILabel!
+    @IBOutlet weak var textView_Notes: UITextView!
+    @IBOutlet weak var button_Save: UIButton!
+    
     // MARK: - Functions
+    
+    private func setupUI() {
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurredEffectView = UIVisualEffectView(effect: blurEffect)
+        self.view.addSubview(blurredEffectView)
+        blurredEffectView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            blurredEffectView.topAnchor.constraint(equalTo: self.imageView_BG.topAnchor),
+            blurredEffectView.bottomAnchor.constraint(equalTo: self.imageView_BG.bottomAnchor),
+            blurredEffectView.leadingAnchor.constraint(equalTo: self.imageView_BG.leadingAnchor),
+            blurredEffectView.trailingAnchor.constraint(equalTo: self.imageView_BG.trailingAnchor)
+        ])
+        
+        self.view.bringSubviewToFront(self.imageView_Banner)
+    }
+    
     // MARK: Overrides
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.setupUI()
     }
 }
