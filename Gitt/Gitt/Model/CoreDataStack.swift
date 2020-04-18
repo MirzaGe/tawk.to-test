faa3bf40b07729faee1c90eb81fa17d40ceaaf0f
Glenn Posadas <hello@glennvon.com>
2020-04-18 22:12:43 +0800

    save core data in the background.

--- Gitt/Gitt/Model/CoreDataStack.swift
+++ Gitt/Gitt/Model/CoreDataStack.swift
@@ -23,7 +23,7 @@ class CoreDataStack {
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "Gitt")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
@@ -39,24 +39,25 @@ class CoreDataStack {
         
         return container
     }()
-
+    
     // MARK: Init
     
     private init() {}
     
     // MARK: - Functions
-
+    
     func saveContext () {
         print("SAVE CONTEXT")
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        persistentContainer.performBackgroundTask { (context) in
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
             }
         }
     }
